e78c52728bcb796224347e1e6c389b79bff47ff8
Filippo <pippo9654@gmail.com>
2026-01-19 14:01:37 +0100

    fix email

--- app/email_notifiche.py
+++ app/email_notifiche.py
@@ -163,7 +163,7 @@ Grazie per aver acquistato da noi!
     try:
         sg = SendGridAPIClient(os.getenv("SENDGRID_API_KEY"))
         message = Mail(
-            from_email="testdev99661@gmail.com",
+            from_email="testdev12311@outlook.com",
             to_emails=recipient,
             subject=subject,
             html_content=html,
